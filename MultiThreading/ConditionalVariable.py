63cf8b211804b8a1fd58dbdabff62e43353ff3fb
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2022-07-10 15:41:52 +0530

    check

---
+++ MultiThreading/ConditionalVariable.py
@@ -0,0 +1,24 @@
+from threading import *
+import time
+
+lock=Lock()
+
+count=0
+
+def task():
+  
+    global count
+    for i in range(1000000):
+        count=count+1
+
+
+if __name__=='__main__':
+    t1=Thread(target=task)
+    t2=Thread(target=task)
+
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    print(count)
+
